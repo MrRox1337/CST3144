e6b745b6d78c5582071de9e4358102386c2e20a6
Aman Mishra <amanrox97@gmail.com>
2024-10-21 10:44:21 +0400

    created server handler

---
+++ Week5/createserver.js
@@ -0,0 +1,13 @@
+var http = require("http"); // Requires the built-in http module
+
+// Defines a function that'll handle incoming HTTP requests
+function requestHandler(request, response) {
+    console.log("Incoming request to: " + request.url);
+    response.end("Hello, world!");
+}
+
+// Creates a server that uses your function to handle requests
+var server = http.createServer(requestHandler);
+
+// Starts the server listening on port 3000
+server.listen(3000);
