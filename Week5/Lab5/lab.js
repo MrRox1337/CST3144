10a6ada46e7e18cb2b9989a2555829994490d7c6
Aman Mishra <amanrox97@gmail.com>
2024-10-23 16:30:13 +0400

    minor updates

--- Week5/Lab5/lab.js
+++ Week5/Lab5/lab.js
@@ -10,8 +10,8 @@ var courses = [
 // Defines a function that’ll handle incoming HTTP requests
 function requestHandler(request, response) {
     console.log("Incoming request to: " + request.url);
-    response.writeHead(200, { "Content-Type": "application/json" });
-    response.end(JSON.stringify(courses));
+    // response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(JSON.stringify(courses, null, 2));
 }
 // Creates a server that uses your function to handle requests
 var server = http.createServer(requestHandler);
