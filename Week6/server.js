81fb2967ecdb1d55092a110b50c106d31427208e
MrRox1337 <amanrox97@gmail.com>
2024-11-10 10:05:08 +0400

    Fixed chalk interaction

--- Week6/server.js
+++ Week6/server.js
@@ -2,40 +2,43 @@ var welcome = require("./generateMessage");
 var { getCurrentTime, getServerStatus } = require("./helper");
 var messages = require("./responses");
 var randomNumber = require("./randomNumber");
-// var chalk = (await import ("chalk")).default;
-var chalk = require("chalk");
 
 var http = require("http"); // Requires the built-in http module
+var chalk;
 
-// Defines a function that'll handle incoming HTTP requests
-function requestHandler(request, response) {
-    console.log("Incoming request to: " + request.url);
-    var welcomeMessage = welcome();
-    var serverResponse = Math.random < 0.5 ? messages.successMessage : messages.errorMessage;
-    var statusNumber = randomNumber();
+(async () => {
+	chalk = (await import("chalk")).default;
 
-    payload = {
-        date_received: getCurrentTime(),
-        server_status: getServerStatus(),
-        message: welcomeMessage,
-        response: serverResponse,
-        statusCode: statusNumber,
-    };
+	// Defines a function that'll handle incoming HTTP requests
+	async function requestHandler(request, response) {
+		console.log("Incoming request to: " + request.url);
+		var welcomeMessage = welcome();
+		var serverResponse = Math.random < 0.5 ? messages.successMessage : messages.errorMessage;
+		var statusNumber = randomNumber();
 
-    console.log(payload);
-    if (statusNumber <= 33) {
-        console.log(chalk.green(`Status Code: ${statusNumber}`));
-    } else if (statusNumber <= 66) {
-        console.log(chalk.yellow(`Status Code: ${statusNumber}`));
-    } else {
-        console.log(chalk.red(`Status Code: ${statusNumber}`));
-    }
-    response.writeHead(200, { "Content-Type": "application/json" });
-    response.end(JSON.stringify(payload));
-}
+		payload = {
+			date_received: getCurrentTime(),
+			server_status: getServerStatus(),
+			message: welcomeMessage,
+			response: serverResponse,
+			statusCode: statusNumber,
+		};
 
-// Creates a server that uses your function to handle requests
-var server = http.createServer(requestHandler);
+		console.log(payload);
+		if (statusNumber <= 33) {
+			console.log(chalk.green(`Status Code: ${statusNumber}`));
+		} else if (statusNumber <= 66) {
+			console.log(chalk.yellow(`Status Code: ${statusNumber}`));
+		} else {
+			console.log(chalk.red(`Status Code: ${statusNumber}`));
+		}
+		response.writeHead(200, { "Content-Type": "application/json" });
+		response.end(JSON.stringify(payload));
+	}
 
-// Starts the server listening on port 3000
-server.listen(3000);
+	// Creates a server that uses your function to handle requests
+	var server = http.createServer(requestHandler);
+
+	// Starts the server listening on port 3000
+	server.listen(3000);
+})();
